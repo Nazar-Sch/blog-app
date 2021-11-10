049764095d05be8d27dc5ed6fffabdcbe10762ef
Nazar Shtypka <nazar.schtypka@gmail.com>
2021-11-10 22:42:53 +0200

    feat/add-prettier

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  trailingComma: 'es5',
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  bracketSpacing: true,
+  printWidth: 75,
+  bracketSameLine: false,
+  arrowParens: 'avoid',
+};
