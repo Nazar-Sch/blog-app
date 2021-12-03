e9753d92739d6e666374c2191dde956bb81d5edd
Nazar Shtypka <nazar.schtypka@gmail.com>
2021-12-03 22:55:58 +0200

    feat: connect mongodb + nodejs + react

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 const express = require("express");
 const config = require("config");
+const mongoose = require("mongoose");
 
 const app = express();
 
-app.get("/", function (req, res) {
-  res.send("Hello World");
-});
+app.use('/api/posts', require('./routes/posts.route'));
 
 const PORT = config.get("port") || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Blog server started on port ${PORT}...`);
-});
+const startApp = async () => {
+  try {
+    await mongoose.connect(config.get("mongoURI"), {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    app.listen(PORT, () =>
+      console.log(`Blog server started on port ${PORT}...`)
+    );
+  } catch (e) {
+    console.log(`Server error: ${e.message}`);
+    process.exit(1);
+  }
+}
+
+startApp();
