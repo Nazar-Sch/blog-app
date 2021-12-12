70c89a90967345079b4e193afcf57d635f722be7
Nazar Shtypka <nazar.schtypka@gmail.com>
2021-12-12 21:45:58 +0200

    feat: actions with post - add editPost api fn

--- client/src/api/posts.ts
+++ client/src/api/posts.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { CreatedPost } from "../types/initialTypes";
 
-const API_LINK = 'api/posts';
+const API_LINK = '/api/posts';
 
 export const getAllPosts = () => axios.get(API_LINK);
 
@@ -10,3 +10,5 @@ export const getSelectedPost = (id: string) => axios.get(`${API_LINK}/${id}`);
 export const addNewPost = (post: CreatedPost) => axios.post(`${API_LINK}/new`, post);
 
 export const deletePostByID = (id: string) => axios.delete(`${API_LINK}/delete/${id}`);
+
+export const editPost = (id: string, post: CreatedPost ) => axios.put(`${API_LINK}/edit/${id}`, post);
